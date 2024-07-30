a53367300bb83d14ca2d687c8dc30da59bad66a3
Abhishe16 <ankushpan694@gmail.com>
2024-07-30 13:17:28 +0530

    updated the app file

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, jsonify
 import pickle
+import pandas as pd
+import difflib
 import numpy as np
+
+app = Flask(__name__)
+
+# Load the dataset
+df = pd.read_csv('movies.csv')
 
 # Load the trained model
 model_path = 'movies_model.pkl'
 with open(model_path, 'rb') as file:
-    model = pickle.load(file)
-
-app = Flask(__name__)
+    similarity = pickle.load(file)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Extract data from form
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    
-    # Make prediction
-    prediction = model.predict(final_features)
-    output = 'Placed' if prediction[0] == 1 else 'Not Placed'
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    movie_name = request.form['movie_name']
+    all_movie_names = df['title'].tolist()
+    find_close_match = difflib.get_close_matches(movie_name, all_movie_names)
 
-    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+    if not find_close_match:
+        return render_template('index.html', recommended_movies=["No match found. Please try again."])
+
+    close_match = find_close_match[0]
+    movie_index = df[df.title == close_match].index[0]
+
+    # Get similarity scores
+    similarity_score = list(enumerate(similarity[movie_index]))
+    sorted_similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+
+    recommended_movies = []
+    for i, movie in enumerate(sorted_similarity_score[1:31]):  # Skip the first movie as it is the input movie itself
+        index = movie[0]
+        title_from_index = df.loc[index, 'title']
+        recommended_movies.append(title_from_index)
+
+    return render_template('index.html', recommended_movies=recommended_movies)
 
 if __name__ == "__main__":
     app.run(debug=True)
